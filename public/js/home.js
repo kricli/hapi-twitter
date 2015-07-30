636ece2d0803214818f150f0c39463439b48fc0f
Richard Li <xpp@me.com>
2015-07-31 02:08:00 +0800

    add read/post tweet deature

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var authenticationData;
   var usersData;
-
+  var tweetsData;
 
   var Signout = function() {
   };
@@ -23,11 +23,20 @@ $(document).ready(function(){
     signout.signoutDelete();
   });
 
-
-
   var loadData = function() {
-
   };
+
+  $('#submitButton').click(function() {
+    $.ajax({
+      method:   'POST',
+      dataType: 'JSON',
+      data:     {tweet: {message: $('#tweetMessage').val()}},
+      url:      'http://localhost:3000/tweets',
+      success:  (function(response){
+        return window.location.reload();
+      }),
+    });
+  });
 
   loadData.prototype.authenticationData = function() {
     $.ajax({
@@ -44,21 +53,58 @@ $(document).ready(function(){
 
   loadData.prototype.usersData = function() {
     $.ajax({
+      context: this,
       method:   'GET',
+      async: false,
       dataType: 'JSON',
       url:      'http://localhost:3000/users',
       success:  (function(response){
         usersData = response;
         console.log(usersData);
-        console.log(($.grep(usersData, function(e){ return e._id == authenticationData.user_id; }))[0].name);
         $('#name').text(($.grep(usersData, function(e){ return e._id == authenticationData.user_id; }))[0].name);
       }),
     });
   };
 
+  loadData.prototype.idToData = function(userID,key){
+    return ($.grep(usersData, function(e){ return e._id == userID; }))[0].key;
+  };
+
+  loadData.prototype.tweetsData = function() {
+    var callBackFunction = function(response) {
+      var html = '';
+      tweetsData = response;
+      console.log(tweetsData);
+      tweetsData.reverse().forEach(function(e){
+        var userID = e.user_id;
+        var name = ($.grep(usersData, function(e){ return e._id == userID; }))[0].name;
+        html +=   "<div class='tweets'>";
+        html +=   "<div class='tweets_name'>";
+        html +=   name;
+        html +=   "</div>";
+        html +=   "<div class='tweets_message'>";
+        html +=   e.message;
+        html +=   "</div>";
+        html +=   "<div class='tweets_date'>";
+        html +=   e.date;
+        html +=   "</div>";
+        html +=   "</div>";
+      });
+      console.log(html);
+      $('#mainTweets').html(html);
+    };
+    $.ajax({
+      method:   'GET',
+      dataType: 'JSON',
+      url:      'http://localhost:3000/tweets',
+      success:  callBackFunction
+    });
+  };
+
+
   var loaddata = new loadData();
   loaddata.authenticationData();
   loaddata.usersData();
-
+  loaddata.tweetsData();
 
 });
